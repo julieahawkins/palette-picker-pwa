e8a943d5b1137c349b9ba1adc94c8eec6c6fdba0
julie hawkins <julabi@gmail.com>
2018-01-23 19:27:34 -0700

    adds isDark check for hex name

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,36 +1,27 @@
-$('.lock-icon').on('click', updateLock);
-$('.colors-btn').on('click', generatePalette);
-
-$(document).on('keyup', (e) => {
-  if (e.keyCode === 32 && e.target === document.body) {
-    generatePalette();
-  }
-});
-
 const paletteColors = [
   {
     div: color1,
-    hex: name1,
+    hexName: name1,
     lock: lock1
   },
   {
     div: color2,
-    hex: name2,
+    hexName: name2,
     lock: lock2
   },
   {
     div: color3,
-    hex: name3,
+    hexName: name3,
     lock: lock3
   },
   {
     div: color4,
-    hex: name4,
+    hexName: name4,
     lock: lock4
   },
   {
     div: color5,
-    hex: name5,
+    hexName: name5,
     lock: lock5
   }
 ];
@@ -39,25 +30,45 @@ function updateLock () {
   $(this).toggleClass('locked');
 };
 
-function generatePalette () {
+const updateHexName = (isDark, hexName) => {
+  const result = isDark ? hexName.addClass('dark') : hexName.removeClass('dark');
+  return result;
+};
+
+const generatePalette = () => {
   paletteColors.forEach(color => {
     if (!$(color.lock).hasClass('locked')) {
-      const newColor = generateColor();
-      $(color.div).css("background", newColor);
-      $(color.hex).text(newColor);
+      const { hex, isDark } = generateColor();
+
+      $(color.div).css("background", hex);
+      $(color.hexName).text(hex);
+      updateHexName(isDark, $(color.hexName));
     }
   });
 };
 
-function generateColor () {
-  return '#'+(Math.random()*0xFFFFFF<<0).toString(16).toUpperCase();
-};
-//chars [0-9, a-f];
-//num = index (random number floored 0 - 16);
-//1st, 3rd and 5th char <23 = dark
-//string is 5 chars plus # at start
-//dark = bool
+const generateColor = () => {
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const nums = letters.map(letter => randomIndex());
+  const chars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ...letters];
+  const hex = `#${chars[nums[0]]}${chars[nums[1]]}${chars[nums[2]]}${chars[nums[3]]}${chars[nums[4]]}${chars[nums[5]]}`;
+  const isDark = (nums[0] + nums[2] + nums[4]) < 25;
 
+  return { hex, isDark };
+};
+
+const randomIndex = () => {
+  return Math.floor(Math.random() * 16);
+};
+
+$('.lock-icon').on('click', updateLock);
+$('.colors-btn').on('click', generatePalette);
+
+$(document).on('keyup', (e) => {
+  if (e.keyCode === 32 && e.target === document.body) {
+    generatePalette();
+  }
+});
 
 
 
