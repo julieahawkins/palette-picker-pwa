4f49aec94da7d7bac125f1bfefc930f4745eb2a4
julie hawkins <julabi@gmail.com>
2018-01-26 09:19:18 -0700

    test file setup

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,2 +1,15 @@
 const knex = require('../db/knex');
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
 
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+
+});
+
+describe('API Routes', () => {
+
+});
